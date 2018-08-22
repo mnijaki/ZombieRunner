0e24270dd20fcbe7105e4a13af6563bc6f130911
mnijaki <mnijaki@gmail.com>
2018-08-22 18:59:58 +0200

    Litte tweaks

---
+++ Assets/General/Scripts/CursorManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+// Cursor manager.
+public class CursorManager : MonoBehaviour
+{
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Serialized fields                  
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // The texture that's going to replace the current cursor.
+  [SerializeField]
+  [Tooltip("The texture that's going to replace the current cursor")]
+  private Texture2D texture;
+
+  #endregion
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Private methods
+  // ---------------------------------------------------------------------------------------------------------------------
+  #region
+
+  // Initialization.
+  private void Start()
+  {
+    // Set cursor.
+    Cursor.SetCursor(this.texture,Vector2.zero,CursorMode.Auto);
+  } // End of Start
+
+  #endregion
+
+} // End of CursorManager
